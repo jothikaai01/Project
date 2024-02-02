00605150828cd379b93365f1d448eaa3fc807ea2
jothikaai01 <jothikajai01@gmail.com>
2024-02-02 11:13:40 +0530

    added the modify file

--- CPLUS/oopsconceptinclpus/main.cpp
+++ CPLUS/oopsconceptinclpus/main.cpp
@@ -2,12 +2,39 @@
 
 using namespace std;
 
-
-
 int main()
 {
+    /*string name;
+    cout << "Enter the Name:" ;
+    cin >> name ;
+    cout << name << endl ;
+    fflush(stdin);
+    cout << "Enter the Second Name:" ;
+    getline(cin,name);
     cout << name << endl;
-    cout << student02::name << endl;
+    */
+
+    string firstname;
+    string secondname;
+    cin >> firstname >> secondname ;
+    cout << firstname+" "+secondname << endl ;
+
+    string fullname = firstname.append(secondname);
+    cout << fullname << endl ;
+
+    secondname.push_back('k');
+    cout << secondname << endl ;
+
+
+    firstname.push_back('SS');
+    cout << firstname << endl ;
+    cout << firstname+" "+secondname << endl ;
+
+    fflush(stdin);
+    cin >> fullname ;
+    fullname.insert(5, " A ");
+    cout << fullname ;
+
     return 0;
 }
 
