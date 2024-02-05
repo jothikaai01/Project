ed35503d209894ef8e87345b72686cc5195fadde
jothikaai01 <jothikajai01@gmail.com>
2024-02-05 08:17:30 +0000

    added the python file

---
+++ Python/Pythondemo/First.py
@@ -0,0 +1,29 @@
+"""mark = 120 ;
+print("if statement") ;
+if mark > 175 :
+    print("Your Pass") ;
+else :
+    print("Your Fail") ;"""
+
+"""mark = 250 ;
+print("Nested if statement") ;
+if mark > 250 :
+    print("Your Pass") ;
+elif mark==250 :
+    print("Your is Passed the exam") ;
+else :
+    print("Your Fail") ;"""
+
+"""a=1 
+print("\nwhileloop statement")
+while a<5 :
+    print(a)
+    a = a + 1 """
+
+a=1 
+print("\nwhileloop statement")
+while a<5 :
+    print(a)
+    if a == 4 :
+        break
+    a = a + 1
