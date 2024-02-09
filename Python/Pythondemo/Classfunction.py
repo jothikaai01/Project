9ee995d88dd1b18ef26a8efb301efa1e178fc9f0
jothikaai01 <jothikajai01@gmail.com>
2024-02-09 13:43:47 +0530

    modify the program in welcome and meeting at school names

--- Python/Pythondemo/Classfunction.py
+++ Python/Pythondemo/Classfunction.py
@@ -6,8 +6,8 @@ class schoolclass:
     def welcome(self):
         print("\n-----------------------------")
         print(self.hm,self.ahm,self.teach)
-x=schoolclass("Jegan","Manjula","Jayshree")
-x.welcome()
+#x=schoolclass("Jegan","Manjula","Jayshree")
+#x.welcome()
 print("-----------------------------")
 
 class sclclass(schoolclass):
@@ -18,5 +18,6 @@ class sclclass(schoolclass):
     def meeting(self):
         print("Good Moring HM",self.hm, "And AHM",self.ahm,"And Teacher",self.teach,"Staff Meeting Arranged by",self.sclplder,"And",self.asssclplder)
 y=sclclass("'Jegan'","'Manjula'","'Jayshree'","'Kowsalya'","'Sandhiya'")
+y.welcome()
 y.meeting()
 print("-----------------------------\n")
