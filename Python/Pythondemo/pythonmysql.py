c33347f2daa93e273f6f784706a391dd41d7aaaf
jothikaai01 <jothikajai01@gmail.com>
2024-02-13 12:34:56 +0530

    added the sql method function show the database and tables etc

---
+++ Python/Pythondemo/pythonmysql.py
@@ -0,0 +1,15 @@
+import mysql.connector
+con=mysql.connector.connect(
+
+    host="192.168.1.240",
+    user="AIBATCH01",
+    password="AI@123",
+    database="j_jothika"
+)
+
+print(con)
+result=con.cursor()
+result.execute("show tables")
+resultshow=result.fetchall()
+for x in resultshow :
+    print(x)
